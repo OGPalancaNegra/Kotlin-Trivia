341b8d029ab44b5b0296d1c259ae8fa36e23f1f5
Alceu Caterça <Alceucaterca@hotmail.com>
2021-06-13 20:37:59 +0100

    Navigation Drawer Attemps

--- app/src/main/java/com/example/kotlintrivia/MainActivity.kt
+++ app/src/main/java/com/example/kotlintrivia/MainActivity.kt
@@ -2,28 +2,59 @@ package com.example.kotlintrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlintrivia.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        //val activityMainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
-       // val navHostFrag: View = findViewById(R.id.myNavHostFragment)
 
 
-        //val navController: NavController = Navigation.findNavController(navHostFrag)
-
-
-
-
+        navView.setNavigationItemSelectedListener(this)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+        }
+        return true
+    }
+
+
+
 }
--- app/src/main/java/com/example/kotlintrivia/categories/CategoryFragment.kt
+++ app/src/main/java/com/example/kotlintrivia/categories/CategoryFragment.kt
@@ -1,13 +1,14 @@
 package com.example.kotlintrivia.categories
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.kotlintrivia.R
 
 import com.example.kotlintrivia.databinding.FragmentCategoryBinding
 import kotlin.collections.toTypedArray as toTypedArray1
@@ -40,17 +41,29 @@ class CategoryFragment : Fragment() {
 
 
 
-
-
         viewModel.navigateToSelectedProperty.observe(viewLifecycleOwner, Observer {
             if (it != null){
                 this.findNavController().navigate(CategoryFragmentDirections.actionCategoryFragmentToAssessmentFragment(it.toTypedArray1()))
                 viewModel.displayQuestionsComplete()
             }
         })
+
+
+        setHasOptionsMenu(true)
         //FragmentCategoryBinding
         // Inflate the layout for this fragment
         return fragmentCategoryBinding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 
